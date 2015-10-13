d4f1180fe3e49dc16c7f028945e3ef8a14a334b7
Shvein Anton <t0hashvein@gmail.com>
2015-10-13 15:51:47 +0500

    nitrogen static removed

---
+++ src/nitrogen_sup.erl
@@ -0,0 +1,29 @@
+%% -*- mode: nitrogen -*-
+%% vim: ts=4 sw=4 et
+-module(nitrogen_sup).
+-behaviour(supervisor).
+-export([
+    start_link/0,
+    init/1
+]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    application:start(crypto),
+    application:start(nprocreg),
+    application:start(simple_bridge),
+
+    {ok, { {one_for_one, 5, 10}, []} }.
