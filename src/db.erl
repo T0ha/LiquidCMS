1f67204b444e0ab2d83d4323b774a6eb86e308ce
Shvein Anton <t0hashvein@gmail.com>
2015-11-30 14:54:17 +0500

    DB module fixed

--- src/db.erl
+++ src/db.erl
@@ -1,38 +1,27 @@
 -module(db).
+%%%
+%%% Mnesia DB backend routines module.
+%%% Put your backend code here
+%%%
+
 -compile([export_all]).
 -include("db.hrl").
 
 -define(V(Response), verify_create_table(Response)).
 
+%% Don't remove! This is is used to install your Mnesia DB backend  from CLI tool
 install([])->
     ok.
 
+%% Don't remove! This is is used to update your Mnesia DB backend  from CLI tool
 update([]) ->
     ok.
                         
-
+%% For convenience in install and update process
 verify_create_table({atomic, ok}) -> ok;
 verify_create_table({aborted, {already_exists, _Table}}) -> ok.
 
-next_id(Table) ->
-    transaction(fun() ->
-                        try 
-                            mnesia:last(Table) + 1
-                        catch
-                            _:_ ->
-                                1      
-                        end
-                end).
-
-remove(Type, Id) -> 
-    transaction(fun() ->
-                         mnesia:delete(Type, Id, write)
-                end).
-write(Rec) ->
-    transaction(fun() ->
-                        mnesia:write(Rec)
-                end).
-
+% Wrapper around Mnesia transaction which returns empty list on errors
 transaction(F) ->
     case mnesia:transaction(F) of
         {atomic, R} -> R;
