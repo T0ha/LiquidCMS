a2c3f4caa6bf5adb9587edf1f8c45feafd1114f8
Shvein Anton <t0hashvein@gmail.com>
2017-07-20 06:54:44 +0500

    Fixed uncommon elements fields trasforming

--- src/cms_modules/admin.erl
+++ src/cms_modules/admin.erl
@@ -438,16 +438,17 @@ add_default_fields(Data, Formatting, Block, Classes) -> % {{{2
       ].
 
 form_fields(M, F, A) -> % {{{2
-    add_default_fields(
       try apply(M, form_data, [F, A])
       catch error:E when E == undef; 
                          E == function_clause -> 
                 [_, Block, Classes] = maybe_empty(A, 3),
                 {[], [], Block, Classes}
-      end).
+      end.
 
 form_elements(M, F, A) -> % {{{2
-    render_fields(form_fields(M, F, A)).
+    render_fields(
+      add_default_fields(
+        form_fields(M, F, A))).
 
 render_fields(Cols) -> % {{{2
     try 12 div length(Cols) of
